34d339b0b531c528786a284f8f0ed1ee25641d77
Fernando Mendes <devfrmendes@gmail.com>
2018-09-09 16:15:09 +0100

    Add reset and normalize.css

--- apps/client/frontend/index.js
+++ apps/client/frontend/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import './reset.css';
+import './normalize.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
