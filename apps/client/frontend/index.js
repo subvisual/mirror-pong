56e0294e954bd64fee2b8fc813e25737c7c8bf36
José Sousa <josesousa9000@gmail.com>
2018-09-16 22:42:20 +0100

    I am not proud of this

--- apps/client/frontend/index.js
+++ apps/client/frontend/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './components/App';
 
 import './reset.css';
 import './normalize.css';
 import './settings.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderReactApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+WebFont.load({
+  google: {
+    families: ['VT323'],
+  },
+  active: renderReactApp,
+});
