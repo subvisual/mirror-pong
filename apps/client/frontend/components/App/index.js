2944e7dbf1c50ef192a052755d2806f5d1531060
José Francisco Sousa <josesousa9000@gmail.com>
2018-09-25 21:30:23 +0100

    Replace parcel with webpack
    Parcel was not behaving correctly on checkouts, and not producing good sourcemaps. In this PR I add Webpack which I believe is the superior bundler.
    
    Scripts remain absolutely the same. I only changed the `package.json` scripts.
    
    I also updated babel to the absolute latest version, as well as every loader that our application can use (babel-loader & css-loader & postcss-loader)

--- apps/client/frontend/components/App/index.js
+++ apps/client/frontend/components/App/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { hot } from 'react-hot-loader';
 import BoardRoom from '../BoardRoom';
 import GameRoom from '../GameRoom';
 import Lobby from '../Lobby';
 
 import './index.css';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <Router>
@@ -20,3 +20,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default hot(module)(App);
---
+++ apps/client/webpack.config.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/index.js',
+
+  stats: {
+    children: false,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[path]__[name]__[local]__[hash:base64:5]',
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+
+  output: {
+    path: path.join(__dirname, '/priv/static/assets'),
+    publicPath: '/assets',
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+  },
+};
