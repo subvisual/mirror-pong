aab1b6032029e058e0676ca258f6dea717559692
José Francisco Sousa <josesousa9000@gmail.com>
2018-10-14 20:44:48 +0100

    Simplify socket connection with an abstraction class using async/await
    * Add babel-polyfill to support async/await generators
    * Create Channel class to simplify socket connection

---
+++ apps/client/frontend/lib/channel.js
@@ -0,0 +1,36 @@
+import { Socket } from 'phoenix';
+
+export default class Channel {
+  constructor(channelname) {
+    this.socket = new Socket('/socket');
+    this.socket.connect();
+    this.channel = this.socket.channel(channelname);
+  }
+
+  join = () =>
+    new Promise((resolve, reject) => {
+      this.channel
+        .join()
+        .receive('ok', response => resolve(response))
+        .receive('error', response => reject(response));
+    });
+
+  leave = () => {
+    const promise = new Promise((resolve, reject) => {
+      this.channel
+        .leave()
+        .receive('ok', response => resolve(response))
+        .receive('error', response => reject(response));
+    });
+
+    promise.finally(() => {
+      this.socket.disconnect();
+    });
+
+    return promise;
+  };
+
+  on = (event, callback) => {
+    this.channel.on(event, callback);
+  };
+}
