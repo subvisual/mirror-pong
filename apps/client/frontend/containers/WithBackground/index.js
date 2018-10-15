8c9d37c8caccb3fabaa4563d3a2f379f5605f105
José Francisco Sousa <josesousa9000@gmail.com>
2018-10-15 13:26:30 +0100

    THE SCORE!
    Why? It's the point of the game.

--- apps/client/frontend/containers/WithBackground/index.js
+++ apps/client/frontend/containers/WithBackground/index.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
+import Scoreboard from '../../components/Scoreboard';
 import Centered from '../../components/Centered';
-
 import Sponsors from './sponsors';
 
 import './index.css';
@@ -27,7 +27,7 @@ export default function(Child) {
       clearInterval(this.interval);
     }
 
-    sponsorLogo() {
+    renderSponsorLogo() {
       const { index } = this.state;
 
       const LogoComponent = Sponsors[index];
@@ -39,14 +39,17 @@ export default function(Child) {
       const { index } = this.state;
 
       const renderLogo =
-        index === Sponsors.length ? 'Mirror Conf' : this.sponsorLogo();
+        index === Sponsors.length ? 'Mirror Conf' : this.renderSponsorLogo();
 
       return (
         <Fragment>
           <div styleName="root">
-            <div styleName="logo">
-              <Centered>{renderLogo}</Centered>
+            <div styleName="score">
+              <Scoreboard />
             </div>
+            <Centered>
+              <div styleName="logo">{renderLogo}</div>
+            </Centered>
           </div>
           <Child {...this.props} />
         </Fragment>
